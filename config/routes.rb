b02e7fe4689820325e6a0450d80da3deceeec2c4
onifernando1 <onifernando1@gmail.com>
2022-09-13 17:33:04 +0100

    posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "posts#new"
-  resources :posts
+  resources :posts only: [:new, :create, :index]
 end

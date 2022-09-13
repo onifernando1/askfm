127fa2534230120f2af760b609371475f41a2146
onifernando1 <onifernando1@gmail.com>
2022-09-13 17:00:49 +0100

    add post routes and controller methods

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+
+    def index
+        @posts = Post.all
+    end
+
+    def show
+        @post = Post.find(params[:id]) 
+    end 
+
+    def new
+        @post = Post.new
+    end 
+
+    def create
+        @post = Post.new
+        if @post.save
+            redirect_to @Post
+        else 
+            render :new, status: :unprocessable_entity
+        end 
+    end 
+end

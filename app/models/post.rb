26be2cb5941f84f5279c5acf5da4a936d60a00a6
onifernando1 <onifernando1@gmail.com>
2022-09-13 22:56:35 +0100

    add validations length body

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
     validates :title, presence: true
     validates :body, presence: true
+    validates :title, length: { maximum: 10 }
+    validates :body, length: { maximum: 50 }
+
     belongs_to :user
 
 

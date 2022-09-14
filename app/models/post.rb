81ec62232e729c0df1bc347f0ac5af0781ac531d
onifernando1 <onifernando1@gmail.com>
2022-09-14 12:54:08 +0100

    center button

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
     validates :title, presence: true
     validates :body, presence: true
-    validates :title, length: { maximum: 10 }
+    validates :title, length: { maximum: 30 }
     validates :body, length: { maximum: 70 }
 
     belongs_to :user
